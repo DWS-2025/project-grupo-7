851ce38c8534038ce4acc1dcac0e5019a25ee4c1
inajerus <a.inarejos2021@gmail.com>
2025-02-12 11:29:17 +0100

    Creacion

---
+++ ProyectoDWS/src/test/java/com/example/proyectodws/ProyectoDwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectodws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
