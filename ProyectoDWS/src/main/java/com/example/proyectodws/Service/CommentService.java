96c08a49fd7e5f5c1b4e2e18bc53bf7005b02335
inajerus <a.inarejos2021@gmail.com>
2025-04-05 12:31:29 +0200

    cambios para meterle

--- ProyectoDWS/src/main/java/com/example/proyectodws/Service/CommentService.java
+++ ProyectoDWS/src/main/java/com/example/proyectodws/Service/CommentService.java
@@ -5,6 +5,7 @@ import com.example.proyectodws.Entities.Comment;
 import com.example.proyectodws.Entities.Course;
 import com.example.proyectodws.Entities.User;
 import com.example.proyectodws.Repository.CommentRepository;
+import com.example.proyectodws.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +18,16 @@ public class CommentService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserSession userSession;
+
     public Optional<Comment> findById(long id) {
         return commentsRepository.findById(id);
     }
 
     public void save(Course courseToComment, Comment comment) {
         courseToComment.getComments().add(comment);
-        User currentUser = userService.getLoggedUser();
+        User currentUser = userService.getUserById(0L);
         comment.setAuthor(currentUser);
         currentUser.getComments().add(comment);
         commentsRepository.save(comment);
--- ProyectoDWS/src/main/java/com/example/proyectodws/Service/SampleDataService.java
+++ ProyectoDWS/src/main/java/com/example/proyectodws/Service/SampleDataService.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-@Service
+/*@Service
 public class SampleDataService {
     @Autowired
     private CourseRepository courseRepository;
@@ -55,3 +55,5 @@ public class SampleDataService {
     }
 
 }
+*/
+
